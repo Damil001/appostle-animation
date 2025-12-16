c1063c8c2574c12d9f9c038925de0920a31daf02
Damil001 <damiljamil63@gmail.com>
2025-12-16 21:36:27 +0500

    Remove asynchronous initialization and error handling from ApostleHero component

--- index-Co2ceBGB.js
+++ index-Co2ceBGB.js
@@ -10474,12 +10474,4 @@ var Nc = document.querySelector(`.particle__container`),
   },
   Lc = new ca(Ic),
   Rc = new sa(Lc);
-Ic.useDebugPanel && (window.apostle_hero = Rc),
-  (async () => {
-    try {
-      await Rc.init(),
-        console.log(`ApostleHero component successfully initialized.`);
-    } catch (e) {
-      console.error(`Failed to initialize ApostleHero component:`, e);
-    }
-  })();
+Ic.useDebugPanel && (window.apostle_hero = Rc);
